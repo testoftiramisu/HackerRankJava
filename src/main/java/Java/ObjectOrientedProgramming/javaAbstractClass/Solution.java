953e4b4fc0493a0759dd4fbeed74c336aed73dfc
Denys Khlivnyy <testoftiramisu@gmail.com>
2016-04-06 23:31:04 +0200

    Solutions for "Java -> Object Oriented Programming"

---
+++ src/main/java/Java/ObjectOrientedProgramming/javaAbstractClass/Solution.java
@@ -0,0 +1,33 @@
+package Java.ObjectOrientedProgramming.javaAbstractClass;
+
+import java.util.*;
+
+abstract class Book {
+    String title;
+
+    abstract void setTitle(String s);
+
+    String getTitle() {
+        return title;
+    }
+
+}
+
+class MyBook extends Book {
+    @Override
+    void setTitle(String s) {
+        this.title = s;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String title = sc.nextLine();
+        MyBook new_novel = new MyBook();
+        new_novel.setTitle(title);
+        System.out.println("The title is: " + new_novel.getTitle());
+
+    }
+}
+

916f14e4c51daecda9fd7d35fa35d2f281dd1a6f
Denys Khlivnyy <testoftiramisu@gmail.com>
2016-04-11 22:15:48 +0200

    Java module done

--- src/main/java/Java/AdvancedChallenges/JavaFactory/Solution.java
+++ src/main/java/Java/AdvancedChallenges/JavaFactory/Solution.java
@@ -1,7 +1,74 @@
 package Java.AdvancedChallenges.JavaFactory;
 
-/**
- * Created by Tiramisu on 4/11/16.
- */
+import java.security.Permission;
+import java.util.Scanner;
+
+interface Food {
+    public String getType();
+}
+
+class Pizza implements Food {
+    public String getType() {
+        return "Someone ordered a Fast Food!";
+    }
+}
+
+class Cake implements Food {
+    public String getType() {
+        return "Someone ordered a Dessert!";
+    }
+}
+
+class FoodFactory {
+    public Food getFood(String order) {
+        if (order.equals("cake")){
+            return new Cake();
+        } else if (order.equals("pizza")){
+            return new Pizza();
+        }
+        return null;
+    }
+}
+
 public class Solution {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String args[]) {
+        Do_Not_Terminate.forbidExit();
+
+        try {
+            //creating the factory
+            FoodFactory foodFactory = new FoodFactory();
+
+            //factory instantiates an object
+            Food food = foodFactory.getFood(scanner.nextLine());
+
+
+            System.out.println("The factory returned " + food.getClass());
+            System.out.println(food.getType());
+        } catch (Do_Not_Terminate.ExitTrappedException e) {
+            System.out.println("Unsuccessful Termination!!");
+        }
+    }
+
+}
+
+class Do_Not_Terminate {
+
+    public static class ExitTrappedException extends SecurityException {
+
+        private static final long serialVersionUID = 1L;
+    }
+
+    public static void forbidExit() {
+        final SecurityManager securityManager = new SecurityManager() {
+            @Override
+            public void checkPermission(Permission permission) {
+                if (permission.getName().contains("exitVM")) {
+                    throw new ExitTrappedException();
+                }
+            }
+        };
+        System.setSecurityManager(securityManager);
+    }
 }
--- src/main/java/Java/AdvancedChallenges/PrimeChecker/Solution.java
+++ src/main/java/Java/AdvancedChallenges/PrimeChecker/Solution.java
@@ -1,7 +1,74 @@
 package Java.AdvancedChallenges.PrimeChecker;
 
-/**
- * Created by Tiramisu on 4/11/16.
- */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
+
+class Prime {
+
+    private boolean isPrime(int number) {
+        if (number == 1) {
+            return false;
+        }
+
+        if (number == 2) {
+            return true;
+        }
+
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void checkPrime(int... varargs) {
+
+        for (int vararg : varargs) {
+            if (isPrime(vararg)) {
+                System.out.print(vararg + " ");
+            }
+        }
+        System.out.println();
+    }
+}
+
 public class Solution {
+
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int n1 = Integer.parseInt(br.readLine());
+            int n2 = Integer.parseInt(br.readLine());
+            int n3 = Integer.parseInt(br.readLine());
+            int n4 = Integer.parseInt(br.readLine());
+            int n5 = Integer.parseInt(br.readLine());
+
+            Prime ob = new Prime();
+            ob.checkPrime(n1);
+            ob.checkPrime(n1, n2);
+            ob.checkPrime(n1, n2, n3);
+            ob.checkPrime(n1, n2, n3, n4, n5);
+
+            Method[] methods = Prime.class.getDeclaredMethods();
+            Set<String> set = new HashSet<>();
+            boolean overload = false;
+            for (int i = 0; i < methods.length; i++) {
+                if (set.contains(methods[i].getName())) {
+                    overload = true;
+                    break;
+                }
+                set.add(methods[i].getName());
+
+            }
+            if (overload) {
+                throw new Exception("Overloading not allowed");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }

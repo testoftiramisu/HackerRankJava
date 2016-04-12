8056b2f447e08339ee628a86dfced15cfe9788ac
Denys Khlivnyy <testoftiramisu@gmail.com>
2016-04-12 23:28:59 +0200

    30 days of code

--- src/main/java/ThirtyDaysOfCode/Day5/Loops/Solution.java
+++ src/main/java/ThirtyDaysOfCode/Day5/Loops/Solution.java
@@ -1,7 +1,15 @@
 package ThirtyDaysOfCode.Day5.Loops;
 
-/**
- * Created by Tiramisu on 4/12/16.
- */
+import java.util.Scanner;
+
 public class Solution {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int num = scanner.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+    }
 }
